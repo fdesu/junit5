02f33527ec80a40742e0a649f3585afaacba9fb4
Christian Stein <sormuras@gmail.com>
2017-09-27 20:25:34 +0200

    Introduce internal predicate wrapper named ClassFilter
    Prior to this commit many methods in classes and interfaces of package
    `org.junit.platform.commons.util` were using the two parameters
    classNameFilter and classTester in their signatures their like:
    
      Predicate<Class<?>> classTester, Predicate<String> classNameFilter
    
    These two parameters can now be put into a ClassFilter instance. This
    will reduce many method signature parts down to
    
      ClassFilter classFilter
    
    and provides a future-proof way of extending the class filter
    implementation without changing the method signatures again.
    
    Addresses #1077

---
+++ junit-platform-commons/src/main/java/org/junit/platform/commons/util/ClassFilter.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2015-2017 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.platform.commons.util;
+
+import static org.apiguardian.api.API.Status.INTERNAL;
+
+import java.util.function.Predicate;
+
+import org.apiguardian.api.API;
+
+/**
+ * Class-related predicate holder used by reflection utilities.
+ *
+ * <h3>DISCLAIMER</h3>
+ *
+ * <p>These utilities are intended solely for usage within the JUnit framework
+ * itself. <strong>Any usage by external parties is not supported.</strong>
+ * Use at your own risk!
+ *
+ * @since 1.1
+ */
+@API(status = INTERNAL, since = "1.1")
+public class ClassFilter implements Predicate<Class<?>> {
+
+	public static ClassFilter of(Predicate<Class<?>> classPredicate) {
+		return of(name -> true, classPredicate);
+	}
+
+	public static ClassFilter of(Predicate<String> namePredicate, Predicate<Class<?>> classPredicate) {
+		Preconditions.notNull(namePredicate, "name predicate must not be null");
+		Preconditions.notNull(classPredicate, "class predicate must not be null");
+
+		return new ClassFilter(namePredicate, classPredicate);
+	}
+
+	private final Predicate<String> namePredicate;
+	private final Predicate<Class<?>> classPredicate;
+
+	private ClassFilter(Predicate<String> namePredicate, Predicate<Class<?>> classPredicate) {
+		this.classPredicate = classPredicate;
+		this.namePredicate = namePredicate;
+	}
+
+	public boolean match(String name) {
+		return namePredicate.test(name);
+	}
+
+	public boolean match(Class<?> type) {
+		return classPredicate.test(type);
+	}
+
+	@Override
+	public boolean test(Class<?> type) {
+		return match(type.getName()) && match(type);
+	}
+}
